6c5eda0a67ddb9388402b6cf80d33d0759e0d384
Nyaya Grah <info@nyayagrah.com>
2026-04-02 17:21:29 +0530

    dds

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol:"https", hostname:"lh3.googleusercontent.com" },
+      { protocol:"https", hostname:"storage.googleapis.com" },
+    ],
+  },
+};
+export default nextConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol:"https", hostname:"lh3.googleusercontent.com" },
-      { protocol:"https", hostname:"storage.googleapis.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "storage.googleapis.com" },
     ],
   },
 };
+
 export default nextConfig;
